1911c2e3871f1e600c20c4f380ad536f35c3eda3
P K Mohan <mohanpanathale18@gmail.com>
2021-08-08 22:38:25 +0530

    first commit

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ app.delete("/user/delete/type/:userType", async (req,res)=>{
     }
 });
 
-app.listen(4000, () =>
+app.listen(process.env.PORT, () =>
 connectDB()
 .then((data) => console.log("Server is running 🚀 "))
 .catch((error) => console.log(error))
